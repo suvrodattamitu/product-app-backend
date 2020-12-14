7ba11aada47030dcea8ee9ea05b14174357e142e
suvro datta <suvrodatta95@gmail.com>
2020-12-14 22:43:00 +0600

    basic bootstrapping and passed tests in jwt login/register via postman

---
+++ routes/api.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::group([
+
+    'prefix'    => 'user',
+    'namespace' => 'User'
+    
+],function(){
+    
+    Route::post('register','AuthController@register');
+    Route::post('login','AuthController@login');
+
+});
